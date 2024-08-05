7ffe57d84f4f209eaa8a8d6bd58a727b3ef9ffba
Meslim <meslimawad@gmail.com>
2024-08-05 22:38:20 +0300

    Removing all the elements from the html and generating it by javascript using a data array contained all the products

---
+++ scripts/amazon.js
@@ -0,0 +1,59 @@
+import {cart, addToCart} from '../data/cart.js';
+import { products } from '../data/products.js';
+let elementHTML = '';
+products.forEach((value) => {
+    const html = `
+    <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${value.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${value.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${value.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${value.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(value.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select class="js-select-quantity">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${value.id}">
+            Add to Cart
+          </button>
+    </div>`
+    
+    elementHTML += html;
+    document.querySelector('.js-product-container')
+    .innerHTML = elementHTML;
+   
+})
